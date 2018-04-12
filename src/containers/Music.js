d3257ca80d9a1da62e0ecc8b56020b777119fef3
xingzo <kingsleynyaosi@gmail.com>
2018-04-12 01:53:21 -0700

    working music component

--- src/containers/Music.js
+++ src/containers/Music.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Music.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class Music extends Component {
   // constructor(props){
@@ -18,7 +19,53 @@ class Music extends Component {
 
   render() {
     return (
-      <div><h2> music me section </h2></div>
+      <div>
+      <main className = "main-container">
+
+    <Row>
+      <Col sm="6">
+        <img className = "album-cover" src="on-my-own.jpg" alt="" />
+        <div className="music-player">
+          <iframe width="100%" height="90" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/372287354&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+
+        </div>
+        <h2 className = "sound-title">Wakanda Mix</h2>
+        <h4>dj shinski</h4>
+      </Col>
+
+      <Col sm="6">
+        <img className = "album-cover" src="on-my-own.jpg" alt="" />
+        <div className="music-player">
+          <iframe width="100%" height="90" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/421343235&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        </div>
+        <h2 className = "sound-title">Wakanda Mix</h2>
+        <h4>dj shinski</h4>
+      </Col>
+    </Row>
+    <Row>
+      <Col sm="6">
+        <img className = "album-cover" src="on-my-own.jpg" alt="" />
+        <div className="music-player">
+          <iframe width="100%" height="90" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/372287354&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+
+        </div>
+        <h2 className = "sound-title">Wakanda Mix</h2>
+        <h4>dj shinski</h4>
+      </Col>
+
+      <Col sm="6">
+        <img className = "album-cover" src="on-my-own.jpg" alt="" />
+        <div className="music-player">
+          <iframe width="100%" height="90" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/421343235&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        </div>
+        <h2 className = "sound-title">Wakanda Mix</h2>
+        <h4>dj shinski</h4>
+      </Col>
+    </Row>
+    <button className="btn btn-primary" >All Sounds</button>
+
+  </main>
+  </div>
     );
   }
 }
