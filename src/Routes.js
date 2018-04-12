fa852b3fc6dfc9e1a24d62937fbb341987292d5f
xingzo <kingsleynyaosi@gmail.com>
2018-04-12 14:01:04 -0700

    form to create a new header image

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import Login from "./containers/Login";
 import Profile from "./containers/Profile";
 import NotFound from "./containers/NotFound";
 import Signup from "./containers/Signup";
+import NewHeader from "./containers/NewHeader";
 import AppliedRoute from "./components/AppliedRoute";
 
 
@@ -13,6 +14,7 @@ export default ({ childProps }) =>
   <Switch>
     <Route path="/" exact component={Home} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/header/new" exact component={NewHeader} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
       <Route path="/profile" exact component={Profile} />
     { /* Finally, catch all unmatched routes */ }
